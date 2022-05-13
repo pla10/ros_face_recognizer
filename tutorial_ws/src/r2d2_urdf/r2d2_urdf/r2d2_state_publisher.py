c95bdf1d6cc53067f8b2a22c8d961b70516325e7
Placido Falqueto <planora@hotmail.com>
2022-05-13 11:16:20 +0200

    Modified r2d2 node to listen to topic /recognize_person

--- tutorial_ws/src/r2d2_urdf/r2d2_urdf/r2d2_state_publisher.py
+++ tutorial_ws/src/r2d2_urdf/r2d2_urdf/r2d2_state_publisher.py
@@ -6,73 +6,91 @@ from geometry_msgs.msg import Quaternion
 from sensor_msgs.msg import JointState
 from tf2_ros import TransformBroadcaster, TransformStamped
 
+from std_msgs.msg import String
+
 class StatePublisher(Node):
 
+    def listener_callback(self, msg):
+        self.recognized = msg.data
+        self.get_logger().info('I heard: "%s"' % msg.data)
+
     def __init__(self):
-        rclpy.init()
         super().__init__('state_publisher')
+        
+        self.recognized = 'None'
 
         qos_profile = QoSProfile(depth=10)
         self.joint_pub = self.create_publisher(JointState, 'joint_states', qos_profile)
         self.broadcaster = TransformBroadcaster(self, qos=qos_profile)
         self.nodeName = self.get_name()
         self.get_logger().info("{0} started".format(self.nodeName))
+        
+        #super().__init__('minimal_subscriber')
+        self.subscription = self.create_subscription(
+            String,
+            'recognized_person',
+            self.listener_callback,
+            10)
+        self.subscription  # prevent unused variable warning
 
-        degree = pi / 180.0
-        loop_rate = self.create_rate(30)
+        self.degree = pi / 180.0
 
         # robot state
-        tilt = 0.
-        tinc = degree
-        swivel = 0.
-        angle = 0.
-        height = 0.
-        hinc = 0.005
+        self.tilt = 0.
+        self.tinc = self.degree
+        self.swivel = 0.
+        self.angle = 0.
+        self.height = 0.
+        self.hinc = 0.005
+        self.motion=0
 
         # message declarations
-        odom_trans = TransformStamped()
-        odom_trans.header.frame_id = 'odom'
-        odom_trans.child_frame_id = 'axis'
-        joint_state = JointState()
+        self.odom_trans = TransformStamped()
+        self.odom_trans.header.frame_id = 'odom'
+        self.odom_trans.child_frame_id = 'axis'
+        self.joint_state = JointState()
+        
+        timer_period = 0.1  # seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
 
-        try:
-            while rclpy.ok():
-                rclpy.spin_once(self)
+    def timer_callback(self):
+        # update flag for motion based on recognized_person topic
+        if self.recognized != 'None':
+        		self.get_logger().info('I heard: "%s"' % self.recognized)
+        		self.motion = 1
+        else:
+        		self.get_logger().info('I heard: "%s"' % self.recognized)
+        		self.motion = 0
 
-                # update joint_state
-                now = self.get_clock().now()
-                joint_state.header.stamp = now.to_msg()
-                joint_state.name = ['swivel', 'tilt', 'periscope']
-                joint_state.position = [swivel, tilt, height]
+        # update joint_state
+        now = self.get_clock().now()
+        self.joint_state.header.stamp = now.to_msg()
+        self.joint_state.name = ['swivel', 'tilt', 'periscope']
+        self.joint_state.position = [self.swivel, self.tilt, self.height]
 
-                # update transform
-                # (moving in a circle with radius=2)
-                odom_trans.header.stamp = now.to_msg()
-                odom_trans.transform.translation.x = cos(angle)*2
-                odom_trans.transform.translation.y = sin(angle)*2
-                odom_trans.transform.translation.z = 0.7
-                odom_trans.transform.rotation = \
-                    euler_to_quaternion(0, 0, angle + pi/2) # roll,pitch,yaw
+        # update transform
+        # (moving in a circle with radius=2)
+        self.odom_trans.header.stamp = now.to_msg()
+        self.odom_trans.transform.translation.x = cos(self.angle)*2
+        self.odom_trans.transform.translation.y = sin(self.angle)*2
+        self.odom_trans.transform.translation.z = 0.7
+        self.odom_trans.transform.rotation = \
+            euler_to_quaternion(0, 0, self.angle + pi/2) # roll,pitch,yaw
 
-                # send the joint state and transform
-                self.joint_pub.publish(joint_state)
-                self.broadcaster.sendTransform(odom_trans)
+        # send the joint state and transform
+        self.joint_pub.publish(self.joint_state)
+        self.broadcaster.sendTransform(self.odom_trans)
 
-                # Create new robot state
-                tilt += tinc
-                if tilt < -0.5 or tilt > 0.0:
-                    tinc *= -1
-                height += hinc
-                if height > 0.2 or height < 0.0:
-                    hinc *= -1
-                swivel += degree
-                angle += degree/4
+        # Create new robot state
+        self.tilt += self.tinc*self.motion
+        if self.tilt < -0.5 or self.tilt > 0.0:
+            self.tinc *= -1
+        self.height += self.hinc*self.motion
+        if self.height > 0.2 or self.height < 0.0:
+            self.hinc *= -1
+        self.swivel += self.degree*self.motion
+        self.angle += self.degree/4*self.motion
 
-                # This will adjust as needed per iteration
-                loop_rate.sleep()
-
-        except KeyboardInterrupt:
-            pass
 
 def euler_to_quaternion(roll, pitch, yaw):
     qx = sin(roll/2) * cos(pitch/2) * cos(yaw/2) - cos(roll/2) * sin(pitch/2) * sin(yaw/2)
@@ -81,8 +99,16 @@ def euler_to_quaternion(roll, pitch, yaw):
     qw = cos(roll/2) * cos(pitch/2) * cos(yaw/2) + sin(roll/2) * sin(pitch/2) * sin(yaw/2)
     return Quaternion(x=qx, y=qy, z=qz, w=qw)
 
+        
+
 def main():
-    node = StatePublisher()
+		rclpy.init()
+
+		node = StatePublisher()
+		rclpy.spin(node)
+		
+		node.destroy_node()
+		rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
